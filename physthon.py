3c18e3146a824d96462f2cf044a73e3003ef9fe4
sfmqur <sammaruska@gmail.com>
2019-01-14 19:15:47 -0600

    added lights to Renderer, added more properties to Sphere

--- physthon.py
+++ physthon.py
@@ -13,8 +13,13 @@ class Renderer:
         self.scene = three.Scene()
 
         # sets up the camera for this renderer
+        # TODO: set up near and far
         self.camera = three.PerspectiveCamera(fov=75, aspect=viewWidth / viewHeight, position=[-10, -6, -10])
         self.scene.add(self.camera)
+
+        self.lights = [three.AmbientLight(), three.DirectionalLight(position=[0, -10, -10])]
+        for l in self.lights:
+            self.scene.add(l)
 
         # sets up the mouse controls of the camera
         self.controller = three.OrbitControls(controlling=self.camera)
@@ -25,10 +30,12 @@ class Renderer:
     def render(self):
         return self.renderer
 
-
+# self.geo needs to be added to Renderer scene
 class Sphere:
-    def __init__(self, size=5, position=[0, 0, 0], color='red', velocity=[0, 0, 0]):
-        self.size = size
+    def __init__(self, radius=5, position=[0, 0, 0], color='red', velocity=[0, 0, 0], acceleration=[0, 0, 0]):
+        self.radius = radius
+        self.geo = three.SphereBufferGeometry(radius=self.radius, widthSegments=10, heightSegments=10)
         self.color = color
         self.pos = position
         self.vel = velocity
+        self.acc = acceleration
