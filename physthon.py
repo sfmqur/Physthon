9d92551b4de46ab4d8155716ca51b236a73fc263
sfmqur <sammaruska@gmail.com>
2019-01-14 18:10:33 -0600

    restructure to utilize classes and objects

--- physthon.py
+++ physthon.py
@@ -5,24 +5,30 @@ import numpy as np
 import pythreejs as three
 from IPython.display import display
 
-#create the scene object which all objects need to be added to
-scene = three.Scene()
+
+class Renderer:
+    def __init__(self, viewWidth=600, viewHeight=400):
+        self.width = viewWidth
+        self.height = viewHeight
+        self.scene = three.Scene()
+
+        # sets up the camera for this renderer
+        self.camera = three.PerspectiveCamera(fov=75, aspect=viewWidth / viewHeight, position=[-10, -6, -10])
+        self.scene.add(self.camera)
+
+        # sets up the mouse controls of the camera
+        self.controller = three.OrbitControls(controlling=self.camera)
+
+        self.renderer = three.Renderer(scene=self.scene, camera=self.camera, width=self.width, height=self.height,
+                                       controls=[self.controller])
+
+    def render(self):
+        return self.renderer
 
 
-# int, int -> renderer
-def renderer(viewWidth = 600, viewHeight = 400):
-    """
-    :param viewWidth: width of simulation window
-    :param viewHeight: height of simulation window
-    :return: threejs renderer object. can run simulation by executing the outputted renderer
-    """
-    camera = three.PerspectiveCamera(fov=75, aspect=viewWidth / viewHeight, position=[10, 6, 10])
-    scene.add(camera)
-
-    # sets up the mouse controls of the camera
-    controller = three.OrbitControls(controlling=camera)
-
-    renderer = three.Renderer(scene=scene, camera=camera, width=viewWidth, height=viewHeight,
-                              controls=[controller])
-
-    return renderer
+class Sphere:
+    def __init__(self, size=5, position=[0, 0, 0], color='red', velocity=[0, 0, 0]):
+        self.size = size
+        self.color = color
+        self.pos = position
+        self.vel = velocity
