494df1d67424905fc69352bea05860b6e52983f5
sfmqur <sammaruska@gmail.com>
2019-01-14 20:40:29 -0600

    optimized light adding to scene, added material and mesh to sphere (now its functional)

--- physthon.py
+++ physthon.py
@@ -14,28 +14,36 @@ class Renderer:
 
         # sets up the camera for this renderer
         # TODO: set up near and far
-        self.camera = three.PerspectiveCamera(fov=75, aspect=viewWidth / viewHeight, position=[-10, -6, -10])
-        self.scene.add(self.camera)
+        self.camera = three.PerspectiveCamera(fov=75, aspect=viewWidth / viewHeight, position=[10, 6, 10], near=0.1,
+                                              far=1000, )
+        # scene takes in a list of things to add
+        self.scene.add([self.camera])
 
-        self.lights = [three.AmbientLight(), three.DirectionalLight(position=[0, -10, -10])]
-        for l in self.lights:
-            self.scene.add(l)
+        self.lights = [three.AmbientLight(), three.DirectionalLight(position=[0, 10, 10])]
+        self.scene.add(self.lights)
 
         # sets up the mouse controls of the camera
         self.controller = three.OrbitControls(controlling=self.camera)
-
         self.renderer = three.Renderer(scene=self.scene, camera=self.camera, width=self.width, height=self.height,
                                        controls=[self.controller])
 
     def render(self):
+        self.renderer = three.Renderer(scene=self.scene, camera=self.camera, width=self.width, height=self.height,
+                                       controls=[self.controller])
         return self.renderer
+
 
 # self.geo needs to be added to Renderer scene
 class Sphere:
-    def __init__(self, radius=5, position=[0, 0, 0], color='red', velocity=[0, 0, 0], acceleration=[0, 0, 0]):
+    # color can be in hex format #ffffff
+    def __init__(self, radius=5, color='red', mass=1, position=[0, 0, 0], velocity=[0, 0, 0], acceleration=[0, 0, 0]):
         self.radius = radius
         self.geo = three.SphereBufferGeometry(radius=self.radius, widthSegments=10, heightSegments=10)
-        self.color = color
+        self.mat = three.MeshPhysicalMaterial(color=color)
+        # TODO: fix it so that calling self (ie scene.add(sphere))returns the self.mesh
+        self.mesh = three.Mesh(geometry=self.geo, material=self.mat)
+
+        self.mass = mass
         self.pos = position
         self.vel = velocity
         self.acc = acceleration
